42c630fe23bc726649631aca91211bba630e7c30
basildybala <akbasil123@gmail.com>
2022-05-11 11:38:41 +0530

    User Updated nd JWT

--- Routes/user.js
+++ Routes/user.js
@@ -1,5 +1,30 @@
 const express=require("express")
 const router=express.Router()
+const {varifyToken,verifyTokenAndAuthorization}=require('./verifyToken')
+const User = require("../models/User");
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) =>{
+    
+    if (req.body.password){
+      req.body.password=CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+    }
+    try{
+        
+        const updatedUser= await User.findByIdAndUpdate(req.params.id,
+            {
+              $set: req.body,
+            },
+            {new: true}
+          );
+          res.status(200).json(updatedUser)
+    }catch(err){
+        res.status(500).json(err)
+    } 
+  });
 
 
 
