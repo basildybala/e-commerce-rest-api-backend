66e7e068b0e1fe6cb2275b7c297a30d915ff4783
basildybala <akbasil123@gmail.com>
2022-05-11 09:34:38 +0530

    e commerce rest api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const connectDB=require('./database/connection');
+const userRoutes=require('./Routes/user.js')
+const orderRoutes=require('./Routes/order')
+const cartRoutes=require('./Routes/cart')
+const productRoutes=require('./Routes/products')
+const authRoutes=require('./Routes/auth')
+
+
+const app=express();
+dotenv.config()
+
+// mongodb connection
+connectDB();
+
+//Sent Json Files In Client Body
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Route SetUp
+app.use('/api/users',userRoutes)
+app.use('/api/auth',authRoutes)
+
+
+
+
+app.listen(process.env.PORT,()=>console.log('Sever Running 5000'))
